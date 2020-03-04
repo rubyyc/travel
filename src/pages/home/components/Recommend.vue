9727fca3addf36c5a01dd71aa4f61393e08fa763
yuanchang <yuanchang2018@gmail.com>
2020-03-04 20:18:57 +0800

    recommend

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,87 @@
+<template>
+  <div>
+    <div class="title">热销</div>
+    <ul>
+      <li class="item border-bottom" v-for="item in recommendList" :key="item.id">
+        <img class="item-img" :src="item.imgUrl" alt="">
+        <div class="item-info">
+          <p class="item-tile">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [
+        {
+          id: '0001',
+          imgUrl: 'https://imgs.qunarzz.com/sight/p0/1609/ca/ca736c64da554ffaa3.water.jpg_200x200_5efad21c.jpg',
+          title: '合肥融创乐园',
+          desc: '合肥融创乐园'
+        },
+        {
+          id: '0002',
+          imgUrl: 'https://imgs.qunarzz.com/sight/p0/1609/ca/ca736c64da554ffaa3.water.jpg_200x200_5efad21c.jpg',
+          title: '合肥融创乐园',
+          desc: '合肥融创乐园'
+        },
+        {
+          id: '0003',
+          imgUrl: 'https://imgs.qunarzz.com/sight/p0/1609/ca/ca736c64da554ffaa3.water.jpg_200x200_5efad21c.jpg',
+          title: '合肥融创乐园',
+          desc: '合肥融创乐园'
+        },
+        {
+          id: '0004',
+          imgUrl: 'https://imgs.qunarzz.com/sight/p0/1609/ca/ca736c64da554ffaa3.water.jpg_200x200_5efad21c.jpg',
+          title: '合肥融创乐园',
+          desc: '合肥融创乐园'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl';
+  .title
+    margin-top .2rem
+    line-height .84rem
+    background #eee
+    text-indent .2rem
+  .item
+    display flex
+    height 1.9rem
+    overflow hidden
+    .item-img
+      width 1.7rem
+      height 1.7rem
+      padding .1rem
+    .item-info
+      flex 1
+      padding .1rem
+      min-width 0
+      .item-title
+        line-height .54rem
+        font-size .32rem
+        ellipsis()
+      .item-desc
+        line-height .4rem
+        color #ccc
+        ellipsis()
+      .item-button
+        line-height .44rem
+        margin-top .16rem
+        background #ff9300
+        padding 0 .2rem
+        border-radius .06rem
+        color #fff
+</style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <div class="title">周末去哪儿</div>
+    <ul>
+      <li class="item border-bottom" v-for="item in recommendList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl" alt="">
+        </div>
+        <div class="item-info">
+          <p class="item-tile">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      recommendList: [
+        {
+          id: '0001',
+          imgUrl: 'https://imgs.qunarzz.com/sight/source/1505/8b/c2fd277d149df.jpg_r_640x214_81b57b6a.jpg',
+          title: '合肥融创乐园',
+          desc: '合肥融创乐园'
+        },
+        {
+          id: '0002',
+          imgUrl: 'https://imgs.qunarzz.com/sight/source/1505/8b/c2fd277d149df.jpg_r_640x214_81b57b6a.jpg',
+          title: '合肥融创乐园',
+          desc: '合肥融创乐园'
+        },
+        {
+          id: '0003',
+          imgUrl: 'https://imgs.qunarzz.com/sight/source/1505/8b/c2fd277d149df.jpg_r_640x214_81b57b6a.jpg',
+          title: '合肥融创乐园',
+          desc: '合肥融创乐园'
+        },
+        {
+          id: '0004',
+          imgUrl: 'https://imgs.qunarzz.com/sight/source/1505/8b/c2fd277d149df.jpg_r_640x214_81b57b6a.jpg',
+          title: '合肥融创乐园',
+          desc: '合肥融创乐园'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl';
+  .title
+    margin-top .2rem
+    line-height .84rem
+    background #eee
+    text-indent .2rem
+  .item-img-wrapper
+    height 0
+    overflow hidden
+    padding-bottom 33.9%
+  .item-img
+    width 100%
+  .item-info
+    padding .1rem
+    .item-title
+      line-height .54rem
+      font-size .32rem
+      ellipsis()
+    .item-desc
+      line-height .4rem
+      color #ccc
+      ellipsis()
+</style>
